afb7c0e9736dc14b12e0bd910f71318d0b4b134b
Dennis <suwatchai.kaew@gmail.com>
2023-12-27 00:18:34 +0700

    feat: add finished ex1-3

--- client/src/components/CreateProductForm.jsx
+++ client/src/components/CreateProductForm.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 function CreateProductForm() {
+  const [description, setDescription] = useState("");
+
+  const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const navigate = useNavigate();
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const newProduct = {
+      name: name,
+      price: price,
+      image: image,
+      description: description,
+    };
+    console.log(newProduct);
+    try {
+      await axios.post("http://localhost:4001/products", newProduct);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
-    <form className="product-form">
+    <form className="product-form" onSubmit={handleSubmit}>
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -8,9 +33,12 @@ function CreateProductForm() {
           <input
             id="name"
             name="name"
+            value={name}
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
           />
         </label>
       </div>
@@ -20,9 +48,12 @@ function CreateProductForm() {
           <input
             id="image"
             name="image"
+            value={image}
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
           />
         </label>
       </div>
@@ -32,9 +63,12 @@ function CreateProductForm() {
           <input
             id="price"
             name="price"
+            value={price}
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
           />
         </label>
       </div>
@@ -44,9 +78,12 @@ function CreateProductForm() {
           <textarea
             id="description"
             name="description"
+            value={description}
             type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
             rows={4}
             cols={30}
           />
--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,12 +1,36 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 function ViewProductPage() {
+  const param = useParams();
+  console.log(param);
+  const id = param.productId;
+  const [data, setData] = useState();
+  async function getData() {
+    try {
+      const response = await axios.get(`http://localhost:4001/products/${id}`);
+      setData(response.data?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, [id]);
+  console.log(param);
+  console.log(data);
   return (
     <div>
       <h1>View Product Page</h1>
       <div className="view-product-container">
-        <h2>Product Title</h2>
-        <p>Content</p>
+        <h2>{data?.name}</h2>
+        <p>{data?.price} THB</p>
+        <p>{data?.description}</p>
       </div>
-      <button>Back to Home</button>
+      <Link to="/">
+        <button>Back to Home</button>
+      </Link>
     </div>
   );
 }
